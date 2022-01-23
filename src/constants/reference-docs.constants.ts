6a09a329d3bff201487a23b62807e69de39abb9b
Chen O <eobender@gmail.com>
2022-01-22 20:26:42 -0800

    Initial relaser

--- src/constants/reference-docs.constants.ts
+++ src/constants/reference-docs.constants.ts
@@ -1,2 +1,2 @@
 export const REF_URL =
-  "https://github.com/rubenspgcavalcante/webpack-extension-reloader/wiki/General-Information";
+  "https://github.com/obender/webpack-extension-reloader-v3-manifest/wiki/General-Information";
